fc9345c6b0bd7cf0ab4b6bc0938d00d0c3d1c64e
Bread <brett.mcbryde@yahoo.com>
2025-03-20 20:18:08 -0500

    base code

---
+++ src/interfaces/Candidate.interface.tsx
@@ -0,0 +1,10 @@
+interface Candidate {
+  login: string;
+  name: string;
+  location: string;
+  email: string;
+  company: string;
+  bio: string;
+  avatar_url: string;
+}
+export default Candidate;
